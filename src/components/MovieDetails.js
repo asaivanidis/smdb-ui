21c91fa74be79068018829f9e97e65e0633b3ba1
saivajr <alexsaivanidis@hotmail.com>
2024-11-18 06:10:14 +0200

    Made movie details page a bit more neat.

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Box, Card, CardContent, Typography, CardMedia, CircularProgress, Button } from '@mui/material';
+import { Box, Card, CardContent, Typography, CardMedia, CircularProgress, Button, Grid2 } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
 import api from '../smdb-api';
 
@@ -63,89 +63,93 @@ const MovieDetails = () => {
 
     return (
         <Box sx={{ padding: '20px' }}>
-            <Card
-                sx={{
-                    maxWidth: 1000,
-                    margin: '0 auto',
-                    backgroundColor: 'background.paper',
-                    padding: 2,
-                    display: 'flex',
-                    flexDirection: { xs: 'column', sm: 'row' }, // Stack vertically on small screens
-                    gap: 2,
-                }}
-            >
-                {/* Movie Poster */}
-                <CardMedia
-                    component="img"
-                    image={movie.coverImageUrl || fallbackImage} // If no url is stored in the database load the fallback image
-                    alt={movie.title}
-                    sx={{
-                        width: { xs: '100%', sm: '300px' },
-                        height: 'auto',
-                        borderRadius: 1,
-                    }}
-                    onError={(e) => {
-                        e.target.onerror = null;
-                        e.target.src = fallbackImage; // If the image cannot be loaded load the fallback image
-                    }} 
-                />
-
-                <CardContent>
-
-                    {/* Movie Title */}
-                    <Typography variant="h5" gutterBottom>
-                        {movie.title}
-                    </Typography>
-
-                    {/* Movie Description */}
-                    <Typography variant="body1" color="text.secondary" paragraph>
-                        {movie.description}
-                    </Typography>
-
-                    {/* Release Date */}
-                    <Typography variant="body2" color="text.secondary">
-                        <strong>Release Date:</strong> {new Date(movie.releaseDate).toLocaleDateString()}
-                    </Typography>
-
-                    {/* Trailer Link */}
-                    {movie.trailerUrl && (
-                        <Typography variant="body2" sx={{ marginTop: 2 }}>
-                            <a
-                                href={movie.trailerUrl}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                style={{ textDecoration: 'none', color: '#03dac6' }}
-                            >
-                                Watch Trailer
-                            </a>
-                        </Typography>
-                    )}
-
-                    {/* Image Carousel */}
-                    {movie.imageUrls && movie.imageUrls.length > 0 && (
-                        <Box sx={{ marginTop: 2 }}>
-                            <Carousel>
-                                {movie.imageUrls.map((url, index) => (
-                                <CardMedia
-                                    key={index}
-                                    component="img"
-                                    height="300"
-                                    image={url}
-                                    alt={`Additional Image ${index + 1}`}
-                                />
-                                ))}
-                            </Carousel>
-                        </Box>
-                    )}
-
-                    {/* Edit Button */}
-                    <Box sx={{ textAlign: 'center', marginTop: 2 }}>
-                        <Button variant="contained" color="primary" onClick={handleEdit}>
-                            Edit Movie
-                        </Button>
-                    </Box>
-                </CardContent>
+            <Card sx={{ maxWidth: 800, margin: '0 auto', padding: 3 }}>
+                <Grid2 container spacing={2}>
+                    <Grid2 item xs={12} sm={4}>
+                        {/* Movie Poster */}
+                        <CardMedia
+                            component="img"
+                            image={movie.coverImageUrl || fallbackImage} // If no url is stored in the database load the fallback image
+                            alt={movie.title}
+                            sx={{
+                                width: { xs: '100%', sm: '300px' },
+                                height: 'auto',
+                                borderRadius: 2,
+                            }}
+                            onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = fallbackImage; // If the image cannot be loaded load the fallback image
+                            }} 
+                        />
+                    </Grid2>
                 
+                 
+                    {/* Movie Details */}
+                    <Grid2 item xs={12} sm={8}>
+                        {/* Movie Title */}
+                        <Typography variant="h5" gutterBottom>
+                            {movie.title}
+                        </Typography>
+
+                        {/* Movie Description */}
+                        <Typography variant="body1" color="text.secondary" component="p" style={{whiteSpace: 'pre-line'}}>
+                            {movie.description}
+                        </Typography>
+
+                        {/* Release Date */}
+                        <Typography variant="body2" color="text.secondary">
+                            <strong>Release Date:</strong> {new Date(movie.releaseDate).toLocaleDateString()}
+                        </Typography>
+
+                        {/* Trailer Link */}
+                        {movie.trailerUrl && (
+                            <Typography variant="body2" sx={{ marginTop: 2 }}>
+                                <a
+                                    href={movie.trailerUrl}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    style={{ textDecoration: 'none', color: '#03dac6' }}
+                                >
+                                    Watch Trailer
+                                </a>
+                            </Typography>
+                        )}
+                        
+                    </Grid2>
+                </Grid2>
+                
+                {/* Images Carousel */}
+                {movie.imageUrls?.length > 0 && (
+                    <Box sx={{ marginTop: 5, paddingLeft: 10, paddingRight: 10 }}> 
+                        <Carousel
+                            autoPlay={true}
+                            animation="slide"
+                            indicators={true}
+                            navButtonsAlwaysVisible={true}
+                            sx={{ borderRadius: 2 }}
+                        >
+                            {movie.imageUrls.map((url, index) => (
+                                <img
+                                    key={index}
+                                    src={url}
+                                    alt={`Image ${index + 1}`}
+                                    style={{
+                                        width: '100%',
+                                        maxHeight: '300px',
+                                        objectFit: 'cover',
+                                        borderRadius: '20px',
+                                    }}
+                                />
+                            ))}
+                        </Carousel>
+                    </Box>
+                )}
+                {/* Edit Button */}
+                <Box sx={{ textAlign: 'center', marginTop: 5 }}>
+                            <Button variant="contained" color="primary" onClick={handleEdit}>
+                                Edit Movie
+                            </Button>
+                        </Box>
             </Card>
         </Box>
     );
